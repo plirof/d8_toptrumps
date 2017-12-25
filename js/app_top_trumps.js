31bbbc55850f099e72cf295ed2979216e488b446
aaa <aaa@aaa.com>
2017-12-25 17:09:16 +0200

    test cards view v004

---
+++ js/app_top_trumps.js
@@ -0,0 +1,82 @@
+
+jQuery(document).ready(function($) {
+
+ var fields_json = (function () {
+    var fields_json = null;
+    $.ajax({
+        'type': 'get',      
+        'async': false,
+        'global': false,
+        'url': "fields.php",
+        'dataType': "json",
+        'success': function (data) {
+            //fields_json = data.fields;
+            fields_json = data;
+        }
+    });
+    return fields_json;
+})(); 
+
+
+
+ var cards_json = (function () {
+    var cards_json = null;
+    $.ajax({
+        'async': false,
+        'type': 'get',
+        'global': false,
+        //'url': "cards.php",
+        //'url': "http://localhost/drupal8test/toptrumps_cards",
+        'url': "http://localhost/drupal8test/toptrumps_cards",
+        'dataType': "json",
+        'success': function (data) {
+            //cards_json = data.cards;
+            cards_json = data;
+        }
+    });
+    return cards_json;
+})(); 
+
+console.log(fields_json +"^^^^^^^^^^^^^^^"+cards_json);
+
+
+      $(document).ready(function() {
+        $('#cardgame').toptrumps({
+          'fields' : fields_json,          
+          'cards' : cards_json,
+          'trick' : 'requeue',
+          'difficulty' : 'easy',
+          'onInit' : function($instance, settings) {
+            var title = 'Top Trumps Cats<br>Start Game';
+            $instance.find('.tt-start-button').html(title);
+          },
+          'onComplete' : function($instance, settings) {
+            var message = '';
+            if (settings.points.cpu > settings.points.user) {
+               message+= '<h2>You have lost!</h2>';
+            }
+            else if (settings.points.cpu < settings.points.user) {
+               message+= '<h2>Congratulations. You win!</h2>';
+            }
+            else {
+               message+= '<h2>Lame. Just a draw.</h2>';
+            }
+
+            message+= '<p>Thanks for playing the jQuery TopTrumps Demo.</p>';
+            message+= '<p>Checkout the repository at <a href="https://github.com/christianhanne/top_trumps">Github</a>.</p>';
+            message+= '<p>Plugin: <a href="http://www.christianhanne.de">Christian Hanne</a></p>';
+            message+= '<p>Drawing: <a href="http://www.sophiesievert.de">Sophie Sievert</a></p>';
+
+            $instance.prepend($('<div>').css('text-align', 'center').html(message));
+          },
+          'renderCard' : function($card, card, fields) {
+            $card.find('.tt-card-fields')
+              .before('<img src="' +  card.image + '" alt="' + card.name + '" width="300" height="300">');
+          }
+        }); // END of $('#cardgame').toptrumps({
+      }); //$(document).ready(function() {
+
+
+}); // END of jQuery(document).ready(function($) {
+
+
---
+++ src/Controller/TopTrumpsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\d8_toptrumps01\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class TopTrumpsController extends ControllerBase {
+
+  public function get_top_trumps_game() {
+    $element = array(
+
+    '#type' => 'inline_template',
+    '#template' => '
+          <table border=2>
+
+<tr><td colspan=1>
+    
+
+
+    
+    <label for="afm">Παρακαλώ επιλέξτε επίθετο εκπαιδευτικού: </label>
+    <div id="selectize">Loading...selectize afm</div><br>
+   </td><td>
+
+       <label for="afm">Παρακαλώ επιλέξτε παραρτημα: </label>
+    <div id="selectize_building">Loading...selectize parartima_id</div><br> 
+    </td></tr>    
+
+
+    
+    </table>
+                  <BR>
+                  <div id="cardgame"></div>
+                  <!--<div id="example-table">Loading... sample-tabulator table</div><br> -->
+                  
+              ',
+      //'#allowed_tags' => ['html'],          
+      '#attached' => array(
+        'library' => array(
+          'jdrupal/jdrupal',
+          'tabulator_workhours/app'
+        )
+      )
+    );
+    return $element;
+  }
+
+}
+?>
